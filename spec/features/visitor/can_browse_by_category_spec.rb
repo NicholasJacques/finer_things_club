a4eca761a53e937c60547c8a340d63c1414010ac
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-04-26 13:07:31 -0600

    adds second test to browse by category spec

--- spec/features/visitor/can_browse_by_category_spec.rb
+++ spec/features/visitor/can_browse_by_category_spec.rb
@@ -14,4 +14,18 @@ RSpec.describe "can visit ':category_name' and browse items for that category", 
     expect(page).to have_content(item_1.price)
     expect(page).to_not have_content(item_2.title)
   end
+
+  scenario "can visit '/land' and view items for that category" do
+    air = create(:category, title: "Air")
+    land = create(:category, title: "Land")
+    item_1 = create(:item, category: air)
+    item_2 = create(:item, category: land)
+
+    visit category_path(land)
+
+    expect(current_path).to eq('/land')
+    expect(page).to have_content(item_2.title)
+    expect(page).to have_content(item_2.price)
+    expect(page).to_not have_content(item_1.title)
+  end
 end
